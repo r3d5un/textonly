27e3bfb85d84f50c848bb1035143d58bbd5f5122
r3d5un <r3d5un@pm.me>
2023-07-17 19:16:06 +0200

    Add basic routes
    Add basic routes

--- main.go
+++ main.go
@@ -6,12 +6,31 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("Hello from textonly"))
+}
+
+func readPost(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from readPost"))
+}
+
+func posts(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from posts"))
+}
+
+func about(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from about"))
 }
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/post/read/", readPost)
+	mux.HandleFunc("/post/", posts)
+	mux.HandleFunc("/about/", posts)
 
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
