c3505709ecf23b1e2acb16505e9b571615cc4c2d
r3d5un <r3d5un@pm.me>
2023-07-17 19:23:44 +0200

    Add URL query parameter to readPost

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
@@ -14,7 +16,13 @@ func home(w http.ResponseWriter, r *http.Request) {
 }
 
 func readPost(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello from readPost"))
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	fmt.Fprintf(w, "Display a specific post with ID %d...", id)
 }
 
 func posts(w http.ResponseWriter, r *http.Request) {
