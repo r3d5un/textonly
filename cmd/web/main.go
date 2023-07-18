f75de23514fe8b6eb03dd92b1f9f36bf2a055437
r3d5un <r3d5un@pm.me>
2023-07-18 17:10:58 +0200

    Add navbar template
    Dark mode
    
    Add about link
    
    Add JavaScript snippet for active links in navbar

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,10 +7,15 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/home/", home)
 	mux.HandleFunc("/post/read/", readPost)
 	mux.HandleFunc("/post/", posts)
-	mux.HandleFunc("/about/", posts)
+	mux.HandleFunc("/about/", about)
 
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
