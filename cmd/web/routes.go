9687881a2ac24b1eed7b66d68c1a95e530f8b9c2
r3d5un <r3d5un@pm.me>
2023-07-21 21:54:09 +0200

    Add middleware for security headers

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,7 +2,7 @@ package main
 
 import "net/http"
 
-func (app *application) routes() *http.ServeMux {
+func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
@@ -14,5 +14,5 @@ func (app *application) routes() *http.ServeMux {
 	mux.HandleFunc("/post/", app.posts)
 	mux.HandleFunc("/about/", app.about)
 
-	return mux
+	return secureHeaders(mux)
 }
