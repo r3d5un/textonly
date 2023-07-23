f6cfa8b41b2d38ef524b1406b09458d25db24d39
r3d5un <r3d5un@pm.me>
2023-07-23 20:45:00 +0200

    Add database model for user information

---
+++ internal/models/user.go
@@ -0,0 +1,38 @@
+package models
+
+import (
+	"database/sql"
+	"errors"
+)
+
+type User struct {
+	ID      int
+	Name    string
+	Summary string
+	Content string
+}
+
+type UserModel struct {
+	DB *sql.DB
+}
+
+func (m *UserModel) Get(id int) (*User, error) {
+	stmt := `
+        SELECT user_id, name, summary, content
+        FROM users
+        WHERE user_id = $1
+        LIMIT 1;
+    `
+	row := m.DB.QueryRow(stmt, id)
+	user := &User{}
+
+	err := row.Scan(&user.ID, &user.Name, &user.Summary, &user.Content)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, ErrNoRecord
+		} else {
+			return nil, err
+		}
+	}
+	return user, nil
+}
