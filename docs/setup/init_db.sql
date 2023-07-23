04cbca04601ad13b2421d69045d1b0cdb753bb41
r3d5un <r3d5un@pm.me>
2023-07-23 18:02:17 +0200

    Update init_db.sql

--- docs/setup/init_db.sql
+++ docs/setup/init_db.sql
@@ -1,3 +1,20 @@
+CREATE TABLE public.users (
+	user_id int8 NOT NULL GENERATED ALWAYS AS IDENTITY( INCREMENT BY 1 MINVALUE 1 MAXVALUE 9223372036854775807 START 1 CACHE 1 NO CYCLE),
+	"name" varchar(100) NOT NULL,
+	summary text NOT NULL,
+	"content" text NOT NULL,
+	CONSTRAINT users_pkey PRIMARY KEY (user_id)
+);
+
+CREATE TABLE public.socials (
+	id bigserial NOT NULL,
+	user_id int8 NOT NULL,
+	social_platform varchar(100) NOT NULL,
+	link varchar(1000) NOT NULL
+);
+
+ALTER TABLE public.socials ADD CONSTRAINT fk_users FOREIGN KEY (user_id) REFERENCES public.users(user_id);
+
 CREATE TABLE public.posts (
 	id bigserial NOT NULL,
 	title varchar(255) NOT NULL,
@@ -7,6 +24,32 @@ CREATE TABLE public.posts (
 	created timestamp NOT NULL,
 	CONSTRAINT posts_pkey PRIMARY KEY (id)
 );
+
+CREATE TABLE public.posts (
+	id bigserial NOT NULL,
+	title varchar(255) NOT NULL,
+	lead text NOT NULL,
+	post text NOT NULL,
+	last_update timestamp NOT NULL,
+	created timestamp NOT NULL,
+	CONSTRAINT posts_pkey PRIMARY KEY (id)
+);
+
+INSERT INTO public.users
+("name", summary, "content")
+VALUES(
+	'John Doe',
+	'Lorem ipsum dolor sit amet.',
+	'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. '
+);
+
+INSERT INTO public.socials
+(id, user_id, social_platform, link)
+VALUES(1, 1, 'LinkedIn', 'https://www.linkedin.com/');
+
+INSERT INTO public.socials
+(id, user_id, social_platform, link)
+VALUES(2, 1, 'GitHub', 'https://www.github.com/');
 
 INSERT INTO public.posts (
 	id,
