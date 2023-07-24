97951d828769fafe15952bee2b95db6bcd5aefd5
r3d5un <r3d5un@pm.me>
2023-07-24 17:19:13 +0200

    Add descriptive log statements to handlers

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -10,11 +10,13 @@ import (
 )
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
+	app.infoLog.Printf("querying blogposts")
 	blogPosts, err := app.blogPosts.LastN(3)
 	if err != nil {
 		app.serverError(w, err)
 		return
 	}
+	app.infoLog.Printf("retrieved %d blogposts", len(blogPosts))
 
 	app.render(w, http.StatusOK, "home.tmpl", &templateData{
 		BlogPosts: blogPosts,
@@ -29,6 +31,7 @@ func (app *application) readPost(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	app.infoLog.Printf("querying for blog post with id %d", id)
 	blogPost, err := app.blogPosts.Get(id)
 	if err != nil {
 		if errors.Is(err, models.ErrNoRecord) {
@@ -38,6 +41,7 @@ func (app *application) readPost(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	}
+	app.infoLog.Printf("retrieved post %d - %s", blogPost.ID, blogPost.Title)
 
 	app.render(w, http.StatusOK, "read.tmpl", &templateData{
 		BlogPost: blogPost,
@@ -45,11 +49,13 @@ func (app *application) readPost(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) posts(w http.ResponseWriter, r *http.Request) {
+	app.infoLog.Printf("querying blogposts")
 	blogPosts, err := app.blogPosts.GetAll()
 	if err != nil {
 		app.serverError(w, err)
 		return
 	}
+	app.infoLog.Printf("retrieved %d blogposts", len(blogPosts))
 
 	app.render(w, http.StatusOK, "posts.tmpl", &templateData{
 		BlogPosts: blogPosts,
@@ -57,17 +63,20 @@ func (app *application) posts(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) about(w http.ResponseWriter, r *http.Request) {
+	app.infoLog.Printf("querying user data")
 	user, err := app.user.Get(1)
 	if err != nil {
 		app.serverError(w, err)
 		return
 	}
 
+	app.infoLog.Printf("querying social data")
 	socials, err := app.sosials.GetByUserID(user.ID)
 	if err != nil {
 		app.serverError(w, err)
 		return
 	}
+	app.infoLog.Printf("retrieved %d socials", len(socials))
 
 	app.render(w, http.StatusOK, "about.tmpl", &templateData{
 		User:    user,
@@ -76,11 +85,13 @@ func (app *application) about(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) feed(w http.ResponseWriter, r *http.Request) {
+	app.infoLog.Printf("querying blogposts")
 	blogPosts, err := app.blogPosts.GetAll()
 	if err != nil {
 		app.serverError(w, err)
 		return
 	}
+	app.infoLog.Printf("retrieved %d blogposts", len(blogPosts))
 
 	app.renderXML(w, http.StatusOK, &templateData{
 		BlogPosts: blogPosts,
