42f18a5aa3702187af877ad6b1db26918e9a1902
r3d5un <r3d5un@pm.me>
2023-08-06 20:54:56 +0200

    Add graceful execution shutdown

--- cmd/web/server.go
+++ cmd/web/server.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"net/http"
 	"os"
 	"os/signal"
@@ -17,19 +19,34 @@ func (app *application) serve(addr string) error {
 		WriteTimeout: 10 * time.Second,
 	}
 
+	shutdownError := make(chan error)
+
 	go func() {
 		quit := make(chan os.Signal, 1)
-
 		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-
 		s := <-quit
 
-		app.infoLog.Printf("caught signal: %s", s.String())
+		app.infoLog.Printf("shutting down server. received signal: %s", s.String())
 
-		os.Exit(0)
+		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+		defer cancel()
+
+		shutdownError <- srv.Shutdown(ctx)
 	}()
 
 	app.infoLog.Printf("staring server: %s", srv.Addr)
 
-	return srv.ListenAndServe()
+	err := srv.ListenAndServe()
+	if !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+
+	err = <-shutdownError
+	if err != nil {
+		return err
+	}
+
+	app.infoLog.Printf("stopped server: %s", srv.Addr)
+
+	return nil
 }
