7af036a6615d008acb6256ae848a20950b5bc115
Øyvind Kristiansen <r3d5un@pm.me>
2023-12-26 17:07:20 +0100

    Add Cobra dependency to the CLI app

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -2,8 +2,30 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	execute()
+}
+
+func execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+var rootCmd = &cobra.Command{
+	Use:   "textonly.islandwind.me",
+	Short: "CLI tools for managing textonly",
+	Long: `textonly-control (toctl) is a collection of tools for managing
+the textonly blog application.
+
+The standard textonly web application does not include any UI for
+administering the application. Instead, toctl is meant to be the primary
+way to manage publish and manage posts.`,
+	Version: "0.1",
 }
--- go.mod
+++ go.mod
@@ -7,12 +7,15 @@ require (
 	github.com/julienschmidt/httprouter v1.3.0
 	github.com/justinas/alice v1.2.0
 	github.com/russross/blackfriday/v2 v2.1.0
+	github.com/spf13/cobra v1.8.0
 	golang.org/x/time v0.3.0
 )
 
 require (
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/crypto v0.14.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
 )
