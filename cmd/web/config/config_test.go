afb96b73d03f5848b1de54b4f1235bbb5627bb56
Øyvind Kristiansen <r3d5un@pm.me>
2023-12-31 19:20:37 +0100

    Configure web app using Viper

---
+++ cmd/web/config/config_test.go
@@ -0,0 +1,30 @@
+package config_test
+
+import (
+	"fmt"
+	"testing"
+
+	"textonly.islandwind.me/cmd/web/config"
+)
+
+func TestNewConfig(t *testing.T) {
+	config, err := config.New()
+	if err != nil {
+		t.Errorf("could not create new config '%v'", err)
+	}
+
+	fmt.Println(config.Database.DSN)
+
+	if config.App.ENV != "development" {
+		t.Errorf("expected 'development', got '%v'", config.App.ENV)
+	}
+	if config.App.URL != ":4000" {
+		t.Errorf("expected ':4000', got '%v'", config.App.URL)
+	}
+	if config.Database.DSN != "postgresql://postgres:postgres@database:5432?database=blog" {
+		t.Errorf(
+			"expected 'postgresql://postgres:postgres@database:5432?database=blog', got '%v'",
+			config.Database.DSN,
+		)
+	}
+}
